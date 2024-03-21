8d025604051e88559f31d5ec8c104915e2143d17
Petren <petren.petren2018@gmail.com>
2024-03-21 19:21:07 +0300

    Сессия 2

--- project/News/admin.py
+++ project/News/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CommentCategory, Author, Post, PostCategory, Category
 
-# Register your models here.
+admin.site.register(CommentCategory)
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Category)
+
--- project/News/views.py
+++ project/News/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Category, Post
 
 # Create your views here.
+
+
+class CategoryList(ListView):
+    model = Category
+    ordering = 'name'
+    template_name = 'CategoryList.html'
+    context_object_name = 'Category'
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-created_at'
+    template_name = 'News.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'News_detail.html'
+    context_object_name = 'post'
